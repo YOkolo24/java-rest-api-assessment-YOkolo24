0474e828982f142ca4f84e4ae613de2cdb784195
Yvonne Okolo <yokolo@hotmail.co.uk>
2023-11-19 14:18:34 +0000

    Edit to Responses class

--- src/main/java/com/cbfacademy/apiassessment/service/Responses.java
+++ src/main/java/com/cbfacademy/apiassessment/service/Responses.java
@@ -5,9 +5,9 @@ public class Responses {
     public static final String success = "Success";
     public static final String fail = "Fail";
     public static final String taskAdded = "Task added sucessfully";
-    public static final String taskAlreadyPresent = "Error - Task Id already present";
+    public static final String taskAlreadyPresent = "Error - task ID already present";
     public static final String taskDetailsUpdated = "Task details updated successfully";
     public static final String taskDeleted = "Task deleted sucessfully";
-    public static final String taskNotFound = "Error - Task not found";
+    public static final String taskNotFound = "Error - task not found";
 
 }
